89ad12900c7ec011dd7675cbf619e20c63e2aed2
aadityaforwork <129250086+aadityaforwork@users.noreply.github.com>
2024-06-22 12:15:33 +0530

    Update dbConnect.js

--- server/mern-server/config/dbConnect.js
+++ server/mern-server/config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const dbConnect = async () => {
     try {
         await mongoose.connect(
-          "mongodb+srv://aadii1502:Am150205@cluster0.3b89bbx.mongodb.net/income-expenses-app?retryWrites=true&w=majority"
+          ""
           
         );
         console.log("Db connected Successfully");
